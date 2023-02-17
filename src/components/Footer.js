775cbf024256021fdb9d1f115539007e53b34bcd
Justine Dave M. Delos reyes <delosreyes.justinedave@gmail.com>
2023-02-17 14:11:33 +0800

    Added version

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import logo from '../logo.svg';
 export default function Footer(){
     return(
         <footer className='footer'>
-            <p className='footer-p'>Made with <img className='icon' width={35} src={logo} alt="react"></img> by <pre>Justine Dave</pre></p>
+            <p className='footer-p'>Made with <img className='icon' width={35} src={logo} alt="react"></img> by <pre>Justine Dave (v1.0.1)</pre></p>
         </footer>
     )
 }
